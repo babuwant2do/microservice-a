eeb1a77666cb34bf0bbe84f0f012c5b1ef6d0cc5
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2019-02-01 15:59:49 +0100

    discover service B and call API of service B using Feign

--- src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/MicroserviceAApplication.java
+++ src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/MicroserviceAApplication.java
@@ -3,9 +3,11 @@ package com.wordpress.babuwant2do.microservice.servicea.microservicea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MicroserviceAApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/web/rest/ServiceAResource.java
+++ src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/web/rest/ServiceAResource.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordpress.babuwant2do.microservice.servicea.microservicea.web.proxy.ServiceBResourceProxy;
+
 @RestController
 @RequestMapping("/api/resource-a")
 public class ServiceAResource {
 	
+	private final ServiceBResourceProxy serviceBResourceProxy;
+	
+	public ServiceAResource(ServiceBResourceProxy serviceBResourceProxy){
+		this.serviceBResourceProxy = serviceBResourceProxy;
+	}
+	
 	@RequestMapping("/hello/{name}")
 	public ResponseEntity<String> hello(@PathVariable String name){
-		return ResponseEntity.ok("Hello "+ name);
+		String welcomeMSg = this.serviceBResourceProxy.welcome(name);
+		return ResponseEntity.ok("Hello "+ name+"\n"+welcomeMSg);
 	}
 }
