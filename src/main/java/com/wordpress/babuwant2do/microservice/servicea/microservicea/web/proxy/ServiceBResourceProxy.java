dadeed962de779547a81fc634f335d7945fc6c6c
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2019-02-11 11:45:15 +0100

    cleanup

--- src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/web/proxy/ServiceBResourceProxy.java
+++ src/main/java/com/wordpress/babuwant2do/microservice/servicea/microservicea/web/proxy/ServiceBResourceProxy.java
@@ -1,13 +1,11 @@
 package com.wordpress.babuwant2do.microservice.servicea.microservicea.web.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name="micro-service-B")
 @RequestMapping("/api/resource-b")
-@Service
 public interface ServiceBResourceProxy {
 	@RequestMapping("/welcome/{name}")
 	public String welcome(@PathVariable String name);
